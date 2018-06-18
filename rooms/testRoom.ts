7ce8b3afc2a188cb05ff329fd55ca2d743e150f5
Vo Trung Nam <nam.votrung@gameloft.com>
2018-06-18 16:47:42 +0700

    + Add test Room

---
+++ rooms/testRoom.ts
@@ -0,0 +1,28 @@
+import { Room } from "colyseus";
+
+export class TestRoom extends Room {
+    // this room supports only 4 clients connected
+    maxClients = 4;
+
+    onInit (options) {
+        console.log("TestRoom created!", options);
+    }
+
+    onJoin (client) {
+        this.broadcast(`${ client.sessionId } joined.`);
+    }
+
+    onLeave (client) {
+        this.broadcast(`${ client.sessionId } left.`);
+    }
+
+    onMessage (client, data) {
+        console.log("TestRoom received message from", client.sessionId, ":", data);
+        this.broadcast(`(${ client.sessionId }) ${ data.message }`);
+    }
+
+    onDispose () {
+        console.log("Dispose BasicRoom");
+    }
+
+}
